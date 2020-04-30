03e2dad4ab0cf105d7ff499cf2955ab17f9174f1
Andrey Makeev <suitedm@gmail.com>
2020-04-30 21:02:10 +0300

    30.04.2020

---
+++ FM_Calculator/app.R
@@ -0,0 +1,47 @@
+#
+# Калькулятор финансового рычага и прочих особенностей
+# Андрей Макеев, 2020, курс Финансового менеджмента в ВШБИ
+# suited@mail.ru
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    h1("Калькулятор финансовых показателей"),
+    p("Задание по дисциплине Финансовый мендежмент, ВШБИ, 2020. Андрей Макеев suited@mail.ru"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
